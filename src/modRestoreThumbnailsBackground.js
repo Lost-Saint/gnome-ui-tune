626ee78500f231fe72e66d61f3e34f8a159ede22
Lost Saint <lost.endanger051@8shield.net>
2026-06-10 02:54:20 -0500

    chore: bump to gnome 50

--- src/modRestoreThumbnailsBackground.js
+++ src/modRestoreThumbnailsBackground.js
@@ -1,40 +1,54 @@
-import {Mod} from './mod.js'
+import { Mod } from "./mod.js";
 
-import {WorkspaceThumbnail} from 'resource:///org/gnome/shell/ui/workspaceThumbnail.js'
-import {BackgroundManager} from  'resource:///org/gnome/shell/ui/background.js'
+import { WorkspaceThumbnail } from "resource:///org/gnome/shell/ui/workspaceThumbnail.js";
+import { BackgroundManager } from "resource:///org/gnome/shell/ui/background.js";
 
 export default class extends Mod {
-    enable() {
-        // Thumbnails on main monitor
-        this.bkp_thumb_init = WorkspaceThumbnail.prototype._init;
-        const _init = this.bkp_thumb_init
-        WorkspaceThumbnail.prototype._init = function(metaWorkspace, monitorIndex) {
-            _init.call(this, metaWorkspace, monitorIndex)
-            this._bgManager = new BackgroundManager({
-                monitorIndex: monitorIndex,
-                container: this._contents,
-                vignette: false
-            });
-        }
+  enable() {
+    // Thumbnails on main monitor
+    this.bkp_thumb_init = WorkspaceThumbnail.prototype._init;
+    const _init = this.bkp_thumb_init;
+    WorkspaceThumbnail.prototype._init = function (
+      metaWorkspace,
+      monitorIndex,
+    ) {
+      _init.call(this, metaWorkspace, monitorIndex);
+      this._bgManager = new BackgroundManager({
+        monitorIndex: monitorIndex,
+        container: this._contents,
+        vignette: false,
+      });
 
-        this.bkp_thumb_onDestroy = WorkspaceThumbnail.prototype._onDestroy;
-        const _onDestroy = this.bkp_thumb_onDestroy
-        WorkspaceThumbnail.prototype._onDestroy = function() {
-            _onDestroy.call(this)
-            if (this._bgManager) {
-                this._bgManager.destroy();
-                this._bgManager = null;
-            }
-        }
+      // Shell 50: when the wallpaper image finishes loading after the
+      // thumbnail is built (cold cache on cold boot, or after resume
+      // invalidates the cache), the Meta.BackgroundActor's preferred
+      // size update no longer always propagates to its parent's
+      // allocation, leaving the thumbnail blank until something else
+      // (e.g. switching workspaces) forces a relayout. Force one
+      // ourselves on every load and on every actor swap.
+      const requeue = () => this._bgManager.backgroundActor?.queue_relayout();
+      this._bgManagerLoadedId = this._bgManager.connect("loaded", requeue);
+      this._bgManagerChangedId = this._bgManager.connect("changed", requeue);
+    };
+
+    this.bkp_thumb_onDestroy = WorkspaceThumbnail.prototype._onDestroy;
+    const _onDestroy = this.bkp_thumb_onDestroy;
+    WorkspaceThumbnail.prototype._onDestroy = function () {
+      _onDestroy.call(this);
+      if (this._bgManager) {
+        this._bgManager.destroy();
+        this._bgManager = null;
+      }
+    };
+  }
+
+  disable() {
+    if (this.bkp_thumb_init) {
+      WorkspaceThumbnail.prototype._init = this.bkp_thumb_init;
     }
 
-    disable() {
-        if (this.bkp_thumb_init) {
-            WorkspaceThumbnail.prototype._init = this.bkp_thumb_init
-        }
-
-        if (this.bkp_thumb_onDestroy) {
-            WorkspaceThumbnail.prototype._onDestroy = this.bkp_thumb_onDestroy
-        }
+    if (this.bkp_thumb_onDestroy) {
+      WorkspaceThumbnail.prototype._onDestroy = this.bkp_thumb_onDestroy;
     }
+  }
 }
